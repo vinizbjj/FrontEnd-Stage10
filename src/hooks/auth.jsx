7b9e66bdbf8ad0f0cfe1bdb66e5d2200e92d8f9c
vinizbjj <viniz.bjj@gmail.com>
2022-11-11 22:56:14 -0300

    localstorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -11,6 +11,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("@rocketnotesstg10:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotesstg10:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
@@ -21,6 +24,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketnotesstg10:user");
+    const token = localStorage.getItem("@rocketnotesstg10:token");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
